7062b9ad9167d15ee8dc3cff178135b234c63421
Srilekhaambori <nagavaralakshmibai111@gmail.com>
2025-07-17 18:12:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import { Circles as Loader } from "react-loader-spinner";
-const LoaderSpinner = () => (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Loader color="#00BFFF" height={100} width={100} />
-    </div>
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export default LoaderSpinner
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
